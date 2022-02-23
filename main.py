b4a8d204640cc97cc0bfe142dde59fcb17476698
Law <100299504+yblaw@users.noreply.github.com>
2022-02-24 02:36:35 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord, os, keep_alive, asyncio, datetime, pytz
+
+
+from discord.ext import tasks, commands
+
+client = commands.Bot(
+  command_prefix=':',
+  self_bot=True
+)
+
+
+
+@client.event
+async def on_connect():
+  await client.change_presence(activity = discord.Streaming(name = "24/7 Online ┊ 6AM - 6PM Online ┊ Late Respond ┊ Apex Legends. ┊ Solo.", url = "https://www.twitch.tv/louxvxx"))
+
+
+keep_alive.keep_alive()
+client.run(os.getenv("TOKEN"), bot=False)
